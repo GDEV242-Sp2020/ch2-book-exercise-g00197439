6c1102b280ec55542bd2e0f176684246bc43e027
g00197439 <60353410+g00197439@users.noreply.github.com>
2020-02-06 09:34:34 -0500

    Added pages field and getter method for it

--- Book.java
+++ Book.java
@@ -11,15 +11,17 @@ class Book
      // The fields.
     private String author;
     private String title;
+    private int pages; //Question 2.85
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;  //Question 2.85
     }
 
     //Question 2.83
@@ -30,6 +32,11 @@ class Book
     //Question 2.83
     public String getTitle() {
         return title;
+    }
+    
+    //Question 2.85
+    public int getPages() {
+        return pages;
     }
     
     //Question 2.84
