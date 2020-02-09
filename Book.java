24688164bcc51d15c17296429c53fb7f30c176b1
g00197439 <60353410+g00197439@users.noreply.github.com>
2020-02-09 10:43:25 -0500

    Added comments for fields and methods

--- Book.java
+++ Book.java
@@ -9,16 +9,15 @@
 class Book
 {
      // The fields.
-    private String author;
-    private String title;
-    private int pages; //Question 2.85
-    private String refNumber; //Question 2.88
-    private int borrowed; //Question 2.91
-    private boolean courseText; //Question 2.92
+    private String author; //The author of the book
+    private String title; //The title of the book
+    private int pages; //Question 2.85; the number of pages in the book
+    private String refNumber; //Question 2.88; the reference number of the book in a library
+    private int borrowed; //Question 2.91; the number of times the book has been borrowed
+    private boolean courseText; //Question 2.92; shows whether or not the book is apart of a course text
 
     /**
-     * Set the author and title fields when this object
-     * is constructed.
+     * Constructor
      */
     public Book(String bookAuthor, String bookTitle, int bookPages, boolean course)
     {
@@ -31,46 +30,74 @@ class Book
     }
 
     //Question 2.83
+    /**
+     * Returns the author of the book
+     */
     public String getAuthor() {
         return author;
     }
     
     //Question 2.83
+    /**
+     * Returns the title of the book
+     */
     public String getTitle() {
         return title;
     }
     
     //Question 2.85
+    /**
+     * Returns the number of pages in the book
+     */
     public int getPages() {
         return pages;
     }
     
     //Question 2.88
+    /**
+     * Returns the reference number of the book
+     */
     public String getRefNumber() {
         return refNumber;
     }
     
     //Question 2.91
+    /**
+     * Returns the number of times the book has been borrowed
+     */
     public int getBorrowed() {
         return borrowed;
     }
     
     //Question 2.92
+    /**
+     * Returns whether or not the book is part of a course text
+     */
     public boolean isCourseText() {
         return courseText;
     }
     
     //Question 2.84
+    /**
+     * Prints the author of the book
+     */
     public void printAuthor() {
         System.out.println(author);
     }
     
     //Question 2.84
+    /**
+     * Prints the title of the book
+     */
     public void printTitle() {
         System.out.println(title);
     }
     
     //Question 2.87
+    /**
+     * Prints all of the details of the book including title, author, number of pages, 
+     * how many times it was borrowed, and the reference number if there is one
+     */
     public void printDetails() {
         System.out.println("Title: " + title + ", Author: " + author +
             ", Pages: " + pages + ", Borrowed " + borrowed + " times");
@@ -78,9 +105,13 @@ class Book
             System.out.println(", Reference Number: " + refNumber);
         } else {
             System.out.println(", Reference Number: ZZZ");
+        }
     }
     
     //Question 2.88 & 2.90
+    /**
+     * Allows the user to set a reference number for the book
+     */
     public void setRefNumber(String ref) {
         if(ref.length() >= 3) {
             refNumber = ref;
@@ -90,6 +121,9 @@ class Book
     }
     
     //Question 2.91
+    /**
+     * Records that the book has been borrowed
+     */
     public void borrow () {
         borrowed++;
     }
